9be2eba3427ebb54d199ae6a966a707ddf614858
CORE LUPO <LUPO-LUCAS@proton.me>
2025-04-25 14:48:43 -0400

    Update and rename aula1.py to Aula1_Print.py

---
+++ Aula1_Print.py
@@ -0,0 +1,17 @@
+"""
+DocString (três aspas duplas)
+Usada geralmente para documentar funções, classes ou até o início de scripts.
+O interpretador ignora esse bloco se não estiver sendo usado como documentação de fato.
+Você pode escrever o que quiser aqui.
+É comum aparecer quando usamos help(função) ou __doc__
+"""
+
+''' Usar para escrever suas notas '''
+# Isso também é uma DocString, mas com aspas simples triplas.
+# Pode ser usada da mesma forma.
+
+# Permite escrever um comentário (linha única)
+print(123)  # Comentário depois do código, na mesma linha
+
+# Comentário acima da instrução
+print(456)
