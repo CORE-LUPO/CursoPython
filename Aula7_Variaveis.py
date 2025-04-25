bc8d134ebe4a4ef7e09536c9e7d4f7716adc9bb6
CORE LUPO <LUPO-LUCAS@proton.me>
2025-04-25 17:26:30 -0400

    Aula Sobre Variaveis

---
+++ Aula7_Variaveis.py
@@ -0,0 +1,16 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# Elas recebem valores que podem ser usados depois no programa.
+
+# Convenção PEP8: nomes de variáveis devem começar com letras minúsculas.
+# Podem conter letras, números e underline (_), mas não devem começar com número.
+
+# Atribuindo valores às variáveis
+nome = 'Luiz'                      # string atribuída à variável 'nome'
+idade = 17                         # número inteiro atribuído à variável 'idade'
+
+# Operação lógica para verificar se a idade é maior ou igual a 18
+maior_de_idade = idade >= 18      # 'maior_de_idade' será True se idade >= 18, senão False
+
+# Exibindo informações com print()
+print('Nome:', nome, 'Idade:', idade)          # imprime o nome e a idade
+print('É maior?', maior_de_idade)              # imprime True ou False dependendo da idade
